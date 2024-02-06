b22a2af10a0d179528813d02078b5b5cd9637851
jahdevelops <elijaheze777@gmail.com>
2024-02-06 10:40:18 -0600

    output errors only in dev

--- src/app.js
+++ src/app.js
@@ -38,8 +38,12 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-  console.error(err.stack);
+  if (req.app.get("env") === "development") {
+    res.locals.error = err;
+    console.error(err.stack);
+  } else {
+    res.locals.error = {};
+  }
 
   // render the error page
   res.status(err.status || 500).json({

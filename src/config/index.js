cb4162ee486aee5c2951bb87924409e19e41fe2a
jahdevelops <elijaheze777@gmail.com>
2024-02-06 10:40:30 -0600

    add configs

--- src/config/index.js
+++ src/config/index.js
@@ -18,7 +18,18 @@ module.exports.sessionSecret = process.env.sessionSecret;
 module.exports.jwtSecret = process.env.jwtSecret;
 module.exports.cookieSecret = process.env.cookieSecret;
 module.exports.NODE_ENV = process.env.NODE_ENV;
+module.exports.serverApiDomain = process.env.serverApiDomain;
+module.exports.mailTokenSecret = process.env.mailTokenSecret;
+module.exports.clientDomain = process.env.clientDomain;
 module.exports.paystackConfig = {
   pubKey: process.env.paystackPubKey,
   privKey: process.env.paystackPrivKey,
+};
+
+module.exports.mail = {
+  host: process.env.mail_host,
+  pass: process.env.mail_pass,
+  service: process.env.mail_service,
+  user: process.env.mail_user,
+  port: process.env.mail_port,
 };

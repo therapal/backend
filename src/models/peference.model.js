46d75114ed7aaffe496f88e62dcde7b377b370f7
jahdevelops <elijaheze777@gmail.com>
2024-02-06 10:41:25 -0600

    add preference model for client role

---
+++ src/models/peference.model.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define("preferences", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    specialisations: DataTypes.JSON, // array of ID referenced from "specialisations" table PK
+    gender: {
+      type: DataTypes.ENUM("male", "female", "neutral"),
+      defaultValue: "neutral",
+    },
+    experienceLevel: DataTypes.ENUM("experienced"),
+    language: DataTypes.STRING,
+    therapyFormat: DataTypes.ENUM("chat", "video"),
+    availability: DataTypes.JSON,
+    minTherapistRating: DataTypes.INTEGER,
+    clientId: {
+      type: DataTypes.UUID,
+      unique: true,
+      references: {
+        model: "users",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "CASCADE",
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  });
+};

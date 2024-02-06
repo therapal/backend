b8320b6f1199b1b4f3e7fda6e57e52febbc6e109
jahdevelops <elijaheze777@gmail.com>
2024-02-06 10:49:59 -0600

    comment out sync db

--- src/models/index.js
+++ src/models/index.js
@@ -40,4 +40,10 @@ Object.keys(db).forEach((modelName) => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+// db.sequelize.sync({ alter: false, force: true }).then(async() => {
+//     console.log("Re-sync done");
+//     // await seed()
+
+// });
+
 module.exports = db;

13b58b31c2a4fb7abb66f3c5304d562f76facff8
jahdevelops <elijaheze777@gmail.com>
2024-02-07 07:50:53 -0600

    add review controller

---
+++ src/controller/review.controller.js
@@ -0,0 +1,20 @@
+const { reviews: Review } = require('../models')
+
+const { ApiError } = require('../utils/errors')
+const { catchAsyncErrors } = require('../routes/middlewares/errors')
+
+module.exports.createTherapistReview = catchAsyncErrors(
+  async (req, res, next) => {
+    if (req.body.review === '') {
+      return next(new ApiError('Review cannot be empty', 400))
+    }
+    await Review.create({
+      clientId: req.user.id,
+      content: req.body.review
+    })
+    return res.status(201).json({
+      success: true,
+      message: 'Review created'
+    })
+  }
+)

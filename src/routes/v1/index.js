40e395ff3152126d862c65d72dd2cc3ed3e5ef3d
jahdevelops <elijaheze777@gmail.com>
2024-02-07 07:47:08 -0600

    rename routes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,15 +1,21 @@
-const router = require("express").Router();
+const router = require('express').Router()
 
-const authRoute = require("./auth.route");
-const clientRoute = require("./client.route");
-const therapistRoute = require("./therapist.route");
-const adminRoute = require("./admin.route");
-const appointmentRoute = require("./appointment.route");
+const authRoute = require('./auth.route')
+const adminRoute = require('./admin.route')
+const appointmentRoute = require('./appointment.route')
+const profileRoute = require('./profile.route')
+const searchRoute = require('./search.route')
+const specialisationRoute = require('./specialisation.route')
+const preferenceRoute = require('./preference.route')
+const reviewRoute = require('./review.route')
 
-router.use("/auth", authRoute);
-router.use("/client", clientRoute);
-router.use("/therapist", therapistRoute);
-router.use("/admin", adminRoute);
-router.use("/appointment", appointmentRoute);
+router.use('/auth', authRoute)
+router.use('/admin', adminRoute)
+router.use('/appointment', appointmentRoute)
+router.use('/profile', profileRoute)
+router.use('/search', searchRoute)
+router.use('/specialisation', specialisationRoute)
+router.use('/preference', preferenceRoute)
+router.use('/review', reviewRoute)
 
-module.exports = router;
+module.exports = router

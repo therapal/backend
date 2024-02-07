04c6a8bdbfebe36679871756e4dddc7321369af2
jahdevelops <elijaheze777@gmail.com>
2024-02-07 07:49:19 -0600

    add models for user preferred languages

---
+++ src/models/language.model.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('languages', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    languageName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    isoCode: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    nativeName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    alternateName: DataTypes.STRING,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+  })
+}
---
+++ src/models/preferredLanguage.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('preferredLanguages', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    languageId: {
+      type: DataTypes.UUID,
+      references: {
+        model: 'languages',
+        key: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    },
+    userId: {
+      type: DataTypes.UUID,
+      references: {
+        model: 'users',
+        key: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    },
+    profficiencyLevel: {
+      type: DataTypes.ENUM('beginner', 'intermediate', 'advanced', 'fluent'),
+      allowNull: false
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+  })
+}

b7aa4dd8432da518147ee166426d57635514c275
jahdevelops <elijaheze777@gmail.com>
2024-02-07 07:47:36 -0600

    add specialisation handlers

--- src/routes/v1/specialisation.route.js
+++ src/routes/v1/specialisation.route.js
@@ -1,4 +1,13 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-module.exports = router;
+const { authenticateUser, validateRole } = require('../middlewares/auth')
+const {
+  addSpecialisation
+} = require('../../controller/specialisation.controller')
+
+router
+  .route('/')
+  .put(authenticateUser, validateRole('therapist'), addSpecialisation)
+
+module.exports = router

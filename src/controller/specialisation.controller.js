a3f15f1fdaa75e775a17dc01aac8720e05d1a554
jahdevelops <elijaheze777@gmail.com>
2024-02-09 12:11:35 -0600

    add get handlers for therapist specialisations

--- src/controller/specialisation.controller.js
+++ src/controller/specialisation.controller.js
@@ -1,10 +1,49 @@
 const {
   specialisations: Specialisation,
   categories: Category,
+  users: User,
 } = require("../models");
 const { catchAsyncErrors } = require("../routes/middlewares/errors");
 
 const { ApiError } = require("../utils/errors");
+
+module.exports.getUserSpecialisations = catchAsyncErrors(
+  async (req, res, next) => {
+    const { userId, page = 1, pageSize = 10 } = req.query;
+    const offset = (page - 1) * pageSize;
+
+    if (!(await User.findByPk(userId))) {
+      return next(new ApiError("User does not exist", 404));
+    }
+
+    const matched = await Specialisation.findAndCountAll({
+      where: { userId },
+      attributes: ["categoryId"],
+      include: {
+        model: Category,
+        attributes: ["id", "title", "description"],
+      },
+      limit: pageSize,
+      offset,
+    });
+    matched.rows = matched.rows.map((i) => {
+      i = i.category;
+      return i;
+    });
+
+    return res.status(200).json({
+      success: true,
+      message:
+        matched.count === 0
+          ? "No specialisations found"
+          : "Specialisations found",
+      data: {
+        rows: matched.rows,
+        currentPage: page,
+      },
+    });
+  },
+);
 
 module.exports.addSpecialisation = catchAsyncErrors(async (req, res, next) => {
   const { categoryId } = req.body;
@@ -15,11 +54,11 @@ module.exports.addSpecialisation = catchAsyncErrors(async (req, res, next) => {
     await Specialisation.findOne({ where: { categoryId, userId: req.user.id } })
   ) {
     return next(
-      new ApiError("Specialisation already added for this user", 400),
+      new ApiError("User already belongs to this specialisation", 400),
     );
   }
   await Specialisation.create({
-    therapistId: req.user.id,
+    userId: req.user.id,
     categoryId,
   });
   res.status(200).json({

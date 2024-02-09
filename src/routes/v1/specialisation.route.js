5dee88e3c9c8a143276e853aa0eba716f7bf64c7
jahdevelops <elijaheze777@gmail.com>
2024-02-09 12:11:09 -0600

    add routes for therapists

--- src/routes/v1/specialisation.route.js
+++ src/routes/v1/specialisation.route.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const {
   addSpecialisation,
+  getUserSpecialisations,
 } = require("../../controller/specialisation.controller");
+const { authenticateUser, validateRole } = require("../middlewares/auth");
 
-router.route("/").put(addSpecialisation);
+router
+  .route("/")
+  .put(authenticateUser, validateRole("therapist"), addSpecialisation)
+  .get(getUserSpecialisations);
 
 module.exports = router;

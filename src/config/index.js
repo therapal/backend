09c4511fdf819797149f3e4cc63c211e9034e2a2
ezecodes <>
2024-06-23 17:08:57 +0100

    capitalize config names

--- src/config/index.js
+++ src/config/index.js
@@ -1,40 +1,33 @@
 require("dotenv").config();
 
-module.exports.database = {
+module.exports.DATABASE = {
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  dialect: "postgres",
+  dialect: "mysql",
+  dialectOptions: {
+    charset: "utf8mb4_unicode_ci",
+  },
   port: process.env.DB_PORT,
   host: process.env.DB_HOST,
 };
 
-module.exports.redisConfig = {
-  host: process.env.redis_host,
-  port: process.env.redis_port,
-};
-
-module.exports.sessionSecret = process.env.sessionSecret;
-module.exports.jwtSecret = process.env.jwtSecret;
-module.exports.cookieSecret = process.env.cookieSecret;
+module.exports.SESSION_SECRET = process.env.SESSION_SECRET;
+module.exports.COOKIE_SECRET = process.env.COOKIE_SECRET;
 module.exports.NODE_ENV = process.env.NODE_ENV;
-module.exports.serverApiDomain = process.env.serverApiDomain;
-module.exports.mailTokenSecret = process.env.mailTokenSecret;
-module.exports.clientDomain = process.env.clientDomain;
-module.exports.paystackConfig = {
-  pubKey: process.env.paystackPubKey,
-  privKey: process.env.paystackPrivKey,
+
+module.exports.PAYSTACK = {
+  pubKey: process.env.PAYSTACK_PUBLIC_KEY,
+  privKey: process.env.PAYSTACK_PRIVATE_KEY,
 };
 
-module.exports.mail = {
-  host: process.env.mail_host,
-  pass: process.env.mail_pass,
-  service: process.env.mail_service,
-  user: process.env.mail_user,
-  port: process.env.mail_port,
+module.exports.NO_REPLY_MAIL_CONFIG = {
+  user: process.env.NO_REPLY_MAIL_USER,
+  pass: process.env.NO_REPLY_MAIL_PASS,
 };
 
-module.exports.treblleConfig = {
-  apiKey: process.env.treblleApiKey,
-  projectId: process.env.treblleProjectId,
+module.exports.MAIL_CONFIG = {
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  service: process.env.MAIL_SERVICE,
 };

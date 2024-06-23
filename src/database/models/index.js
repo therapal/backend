e4f1ef11412623fa14919f97f1d48f0aa87f6152
ezecodes <>
2024-06-23 18:56:22 +0100

    update model assoc

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const Sequelize = require("sequelize");
 // eslint-disable-next-line no-undef
 const basename = path.basename(__filename);
-const { DATABASE } = require("../config/index.js");
+const { DATABASE } = require("@config/index.js");
 
 const db = {};
 
@@ -59,35 +59,31 @@ const {
   Messages,
   Reviews,
   Payments,
-  Invoices,
   TherapistCategories,
   Therapists,
   Clients,
 } = db;
 
 Therapists.hasMany(Appointments, { foreignKey: "therapist_id" });
-Appointments.Therapists(User, { foreignKey: "therapist_id" });
+Appointments.belongsTo(Therapists, { foreignKey: "therapist_id" });
 
 Therapists.hasMany(Reviews, { foreignKey: "therapist_id" });
-Reviews.Therapists(User, { foreignKey: "therapist_id" });
+Reviews.belongsTo(Therapists, { foreignKey: "therapist_id" });
 
 Therapists.hasMany(TherapistCategories, { foreignKey: "therapist_id" });
-TherapistCategories.Therapists(User, { foreignKey: "therapist_id" });
+TherapistCategories.belongsTo(Therapists, { foreignKey: "therapist_id" });
 
 Categories.hasMany(TherapistCategories, { foreignKey: "category_id" });
-TherapistCategories.Categories(User, { foreignKey: "category_id" });
+TherapistCategories.belongsTo(Categories, { foreignKey: "category_id" });
 
 Clients.hasMany(Appointments, { foreignKey: "client_id" });
-Appointments.Clients(User, { foreignKey: "client_id" });
+Appointments.belongsTo(Clients, { foreignKey: "client_id" });
 
 Clients.hasMany(Reviews, { foreignKey: "client_id" });
-Reviews.Clients(User, { foreignKey: "client_id" });
+Reviews.belongsTo(Clients, { foreignKey: "client_id" });
 
 Appointments.hasMany(Messages, { foreignKey: "appointment_id" });
 Messages.belongsTo(Appointments, { foreignKey: "appointment_id" });
-
-Payments.hasOne(Invoices, { foreignKey: "invoice_id" });
-Invoices.belongsTo(Payments, { foreignKey: "invoice_id" });
 
 Payments.hasOne(Appointments, { foreignKey: "payment_id" });
 Appointments.belongsTo(Payments, { foreignKey: "payment_id" });
